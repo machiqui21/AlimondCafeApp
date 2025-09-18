6b63cd32aeb100a3c571b91995e127bf3368e194
machiqui21 <machiqui_21@yahoo.com>
2025-09-18 22:14:08 +1200

    Fix Issue on Menu

---
+++ dbConfig.js
@@ -0,0 +1,16 @@
+// MySQL connection setup
+
+var mysql = require('mysql');
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', 
+    password: '',
+    database: 'alimondcafe' 
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected to MySQL database!');
+});
+
+module.exports = db;
