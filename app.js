aa063570677541673a7c49525f4f8308c3efcb92
machiqui21 <machiqui_21@yahoo.com>
2025-10-04 18:36:50 +1300

    fix the issue of misalignment

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 console.log("App started");
+
 var express = require('express');
-var app = express();    
+var app = express();
+var i18n = require('i18n');
 app.set('view engine', 'ejs');
 app.use(express.static('styles'));
 var db = require('./dbConfig');
 
+i18n.configure({
+        locales: ['en', 'tl'],
+        directory: __dirname + '/locales',
+        defaultLocale: 'en',
+        cookie: 'lang',
+        queryParameter: 'lang'
+});
+app.use(i18n.init);
+
 app.use('/images', express.static('images'));
 
 app.get('/', function(req, res) {
-        res.render("homepage");
+        const productQuery = "SELECT * FROM products WHERE category = 'Standard'";
+        const priceQuery = "SELECT * FROM pricelist WHERE category = 'Standard'";
+        const extraQuery = "SELECT * FROM pricelist WHERE category = 'Extras'";
+        db.query(productQuery, function(err, products) {
+                if (err) {
+                        console.error('Product query error:', err);
+                        return res.status(500).send('Database error');
+                }
+                db.query(priceQuery, function(err, pricelist) {
+                        if (err) {
+                                console.error('Price query error:', err);
+                                return res.status(500).send('Database error');
+                        }
+                        db.query(extraQuery, function(err, extraPricelist) {
+                                if (err) {
+                                        console.error('Extra Price query error:', err);
+                                        return res.status(500).send('Database error');
+                                }
+                                res.render('homepage', { ProductData: products, PriceData: pricelist, ExtraPriceData: extraPricelist, __: req.__ });
+                        });
+                });
+        });
 });
 
 app.get('/menu', function(req, res) {
@@ -35,7 +67,16 @@ app.get('/menu', function(req, res) {
                                         return res.status(500).send('Database error');
                                 }
                                 console.log('Extra Pricelist:', extraPricelist);
-                                res.render('menu', { ProductData: products, PriceData: pricelist, ExtraPriceData: extraPricelist });
+                                // Convert RowDataPacket arrays to plain objects
+                                const plainProducts = products.map(row => ({ ...row }));
+                                const plainPricelist = pricelist.map(row => ({ ...row }));
+                                const plainExtraPricelist = extraPricelist.map(row => ({ ...row }));
+                                                                console.log('Sending to EJS:', {
+                                                                        ProductData: plainProducts,
+                                                                        PriceData: plainPricelist,
+                                                                        ExtraPriceData: plainExtraPricelist
+                                                                });
+                                                                res.render('menu', { ProductData: plainProducts, PriceData: plainPricelist, ExtraPriceData: plainExtraPricelist, __: req.__ });
                         });
                 });
         });
